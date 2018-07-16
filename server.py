d7580d56d7fb5f057698475b66b863ee4405e197
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-16 16:54:40 -0700

    Created register form

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,7 +22,41 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route('/users')
+def user_list():
+    """Show list of users"""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route('/register', methods=["GET"])
+def register_form():
+    """Registration page for new users"""
+    return render_template('register_form.html')
+
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    """Processes registration. Checks if user exists, if not adds to db """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if (User.query.filter(User.email == email).all()):
+        flash("A user with that email already exists!")
+
+    else:
+        # Make a new user
+        new_user = User(email=email, password=password)
+        print(new_user)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("You have sucessfully registered!")
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
